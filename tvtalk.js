f7aa738fa3d7cd1e04a31158c419e059c51d6eb0
Skavrx <JohnnDashPM@gmail.com>
2016-11-29 18:57:53 -0500

    things

--- tvtalk.js
+++ tvtalk.js
@@ -1,16 +1,34 @@
-const Discord = require('discord.js');
-const bot = new Discord.Client();
+var Discord = require('discord.js');
+var bot = new Discord.Client();
 
 var config = require('./config.json');
 
+var shows = require('./shows.json');
+
+for (var key in shows) {
+    if (shows.hasOwnProperty(key)) {
+        console.log(key + " -> " + JSON.stringify(shows[key]));
+    }
+}
+
 bot.on('ready', () => {
-  console.log('I am ready!');
+    var boot = ["I am ready to be of service.", "Alert and online, captain.", "Feels good to be online again."];
+    console.log(boot[Math.floor(Math.random() * boot.length)]);
+});
+
+bot.on('serverNewMember', (server, user) => {
+    if (user.bot) return;
 });
 
 bot.on('message', message => {
-  if (message.content === 'ping') {
-    message.reply('pong');
-  }
+    if (message.content === 'ping') {
+        message.reply('pong');
+        var roles = bot.guilds.get("id", config.serverID).roles;
+        console.log(roles);
+        for (var i = 0; i < roles.length; i++) {
+            console.log(roles[i].name + " -> " + roles[i].id + " -> " + roles[i].position + " -> " + roles[i].colorAsHex());
+        }
+    }
 });
 
 bot.login(config.token);
