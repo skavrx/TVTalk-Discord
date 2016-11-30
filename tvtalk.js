fd1fb0a8ca26c6ef484238f9bd947470383a8e38
Skavrx <skavrx@gmail.com>
2016-11-29 21:27:48 -0500

    stuff

--- tvtalk.js
+++ tvtalk.js
@@ -1,34 +1,250 @@
+var fs = require('fs');
+
 var Discord = require('discord.js');
 var bot = new Discord.Client();
 
 var config = require('./config.json');
 
-var shows = require('./shows.json');
+var shows = {};
 
-for (var key in shows) {
-    if (shows.hasOwnProperty(key)) {
-        console.log(key + " -> " + JSON.stringify(shows[key]));
+try {
+    shows = require('./shows.json');
+} catch (e) {
+
+}
+
+// Load custom permissions
+var dangerousCommands = [""];
+var Permissions = {};
+try {
+    Permissions = require("./permissions.json");
+} catch (e) {
+    Permissions.global = {};
+    Permissions.users = {};
+}
+
+for (var i = 0; i < dangerousCommands.length; i++) {
+    var cmd = dangerousCommands[i];
+    if (!Permissions.global.hasOwnProperty(cmd)) {
+        Permissions.global[cmd] = false;
     }
+}
+Permissions.checkPermission = function(user, permission) {
+    try {
+        var allowed = true;
+        try {
+            if (Permissions.global.hasOwnProperty(permission)) {
+                allowed = Permissions.global[permission] === true;
+            }
+        } catch (e) {}
+        try {
+            if (Permissions.users[user.id].hasOwnProperty(permission)) {
+                allowed = Permissions.users[user.id][permission] === true;
+            }
+        } catch (e) {}
+        return allowed;
+    } catch (e) {}
+    return false;
+}
+fs.writeFile("./permissions.json", JSON.stringify(Permissions, null, 2));
+
+var aliases;
+
+var commands = {
+    "aliases": {
+        description: "lists all recorded aliases",
+        process: function(bot, msg, suffix) {
+            var text = "current aliases:\n";
+            for (var a in aliases) {
+                if (typeof a === 'string')
+                    text += a + " ";
+            }
+            msg.channel.sendMessage(text);
+        }
+    },
+    "alias": {
+        usage: "<name> <actual command>",
+        description: "Creates command aliases. Useful for making simple commands on the fly",
+        process: function(bot, msg, suffix) {
+            var args = suffix.split(" ");
+            var name = args.shift();
+            if (!name) {
+                msg.channel.sendMessage(config.prefix + "alias " + this.usage + "\n" + this.description);
+            } else if (commands[name] || name === "help") {
+                msg.channel.sendMessage("overwriting commands with aliases is not allowed!");
+            } else {
+                var command = args.shift();
+                aliases[name] = [command, args.join(" ")];
+                //now save the new alias
+                require("fs").writeFile("./alias.json", JSON.stringify(aliases, null, 2), null);
+                msg.channel.sendMessage("created alias " + name);
+
+            }
+        }
+    },
+    "listshows": {
+        usage: "",
+        description: "",
+        process: function(bot, msg, suffix) {
+            var showlist = '';
+            var count = 1;
+            for (var key in shows) {
+                if (shows.hasOwnProperty(key)) {
+                    //console.log(key + " -> " + JSON.stringify(shows[key]));
+                    showlist += `**${count}.** ${shows[key].name}\n`;
+                    count++;
+                }
+            }
+            msg.channel.sendMessage(showlist).then(message => message.delete(10000)).then(msg.delete(10000));
+        }
+    },
+    "addshow": {
+      usage: "",
+      description: "",
+      process: function(bot, msg, suffix) {
+        fs.writeFile("./shows.json", JSON.stringify(Permissions, null, 2));
+      }
+    },
+    "editshow": {
+
+    },
+    "request": {
+      
+    }
+};
+
+try{
+	aliases = require("./alias.json");
+} catch(e) {
+	//No aliases defined
+	aliases = {};
 }
 
 bot.on('ready', () => {
     var boot = ["I am ready to be of service.", "Alert and online, captain.", "Feels good to be online again."];
-    console.log(boot[Math.floor(Math.random() * boot.length)]);
+    console.log(`${bot.user.username} (${bot.user.id}) - ` + boot[Math.floor(Math.random() * boot.length)]);
 });
 
-bot.on('serverNewMember', (server, user) => {
-    if (user.bot) return;
+bot.on('guildMemberAdd', member => {
+    if (member.user.bot) return;
+    member.guild.defaultChannel.sendMessage(`**New User: <@${member.user.id}>**`)
+        .then(message => console.log(`Sent message: ${message.content}`))
+        .catch(console.error);
+    console.log(member.guild.roles);
+    member.addRole(member.guild.roles.find("name", "temp"));
 });
 
-bot.on('message', message => {
-    if (message.content === 'ping') {
-        message.reply('pong');
-        var roles = bot.guilds.get("id", config.serverID).roles;
-        console.log(roles);
-        for (var i = 0; i < roles.length; i++) {
-            console.log(roles[i].name + " -> " + roles[i].id + " -> " + roles[i].position + " -> " + roles[i].colorAsHex());
+function checkMessageForCommand(msg, isEdit) {
+    //check if message is a command
+    if (msg.author.id != bot.user.id && (msg.content[0] === config.prefix)) {
+        console.log("treating " + msg.content + " from " + msg.author + " as command");
+        var cmdTxt = msg.content.split(" ")[0].substring(1);
+        var suffix = msg.content.substring(cmdTxt.length + 2); //add one for the ! and one for the space
+        if (msg.isMentioned(bot.user)) {
+            try {
+                cmdTxt = msg.content.split(" ")[1];
+                suffix = msg.content.substring(bot.user.mention().length + cmdTxt.length + 2);
+            } catch (e) { //no command
+                msg.channel.sendMessage("Yes?");
+                return;
+            }
+        }
+        alias = aliases[cmdTxt];
+        if (alias) {
+            console.log(cmdTxt + " is an alias, constructed command is " + alias.join(" ") + " " + suffix);
+            cmdTxt = alias[0];
+            suffix = alias[1] + " " + suffix;
+        }
+        var cmd = commands[cmdTxt];
+        if (cmdTxt === "help") {
+            //help is special since it iterates over the other commands
+            if (suffix) {
+                var cmds = suffix.split(" ").filter(function(cmd) {
+                    return commands[cmd]
+                });
+                var info = "";
+                for (var i = 0; i < cmds.length; i++) {
+                    var cmd = cmds[i];
+                    info += "**" + config.prefix + cmd + "**";
+                    var usage = commands[cmd].usage;
+                    if (usage) {
+                        info += " " + usage;
+                    }
+                    var description = commands[cmd].description;
+                    if (description instanceof Function) {
+                        description = description();
+                    }
+                    if (description) {
+                        info += "\n\t" + description;
+                    }
+                    info += "\n"
+                }
+                msg.channel.sendMessage(info);
+            } else {
+                msg.author.sendMessage("**Available Commands:**").then(function() {
+                    var batch = "";
+                    var sortedCommands = Object.keys(commands).sort();
+                    for (var i in sortedCommands) {
+                        var cmd = sortedCommands[i];
+                        var info = "**" + config.prefix + cmd + "**";
+                        var usage = commands[cmd].usage;
+                        if (usage) {
+                            info += " " + usage;
+                        }
+                        var description = commands[cmd].description;
+                        if (description instanceof Function) {
+                            description = description();
+                        }
+                        if (description) {
+                            info += "\n\t" + description;
+                        }
+                        var newBatch = batch + "\n" + info;
+                        if (newBatch.length > (1024 - 8)) { //limit message length
+                            msg.author.sendMessage(batch);
+                            batch = info;
+                        } else {
+                            batch = newBatch
+                        }
+                    }
+                    if (batch.length > 0) {
+                        msg.author.sendMessage(batch);
+                    }
+                });
+            }
+        } else if (cmd) {
+            if (Permissions.checkPermission(msg.author, cmdTxt)) {
+                try {
+                    cmd.process(bot, msg, suffix, isEdit);
+                } catch (e) {
+                    var msgTxt = "command " + cmdTxt + " failed :(";
+                    if (config.debug) {
+                        msgTxt += "\n" + e.stack;
+                    }
+                    msg.channel.sendMessage(msgTxt);
+                }
+            } else {
+                msg.channel.sendMessage("You are not allowed to run " + cmdTxt + "!");
+            }
+        } else {
+            msg.channel.sendMessage(cmdTxt + " not recognized as a command!").then((message => message.delete(5000)))
+        }
+    } else {
+        //message isn't a command or is from us
+        //drop our own messages to prevent feedback loops
+        if (msg.author == bot.user) {
+            return;
+        }
+
+        if (msg.author != bot.user && msg.isMentioned(bot.user)) {
+            msg.channel.sendMessage(msg.author + ", you called?");
+        } else {
+
         }
     }
+}
+
+bot.on('message', msg => {
+    checkMessageForCommand(msg, false);
 });
 
 bot.login(config.token);
